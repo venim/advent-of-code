84f788192cbd376820e94e4335d69d4e6a61beec
Dan Cohen <danrcohen@gmail.com>
2023-09-24 21:07:55 -0500

    2021 day 01.1

---
+++ 2021/01/01.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+var (
+	//go:embed in.txt
+	input string
+	dirs  = map[byte]pos{'>': {0, 1}, '<': {0, -1}, '^': {-1, 0}, 'v': {1, 0}}
+)
+
+func mustAtoi(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+type pos struct {
+	X, Y int
+}
+
+func part1(lines []string) (res int) {
+	n := 0
+	prev := -1
+	for _, _depth := range lines {
+		depth := mustAtoi(_depth)
+		if depth > prev && prev != -1 {
+			n++
+		}
+		prev = depth
+	}
+	return n
+}
+
+func part2(lines []string) (res int) {
+	return
+}
+
+func init() {
+	flag.Set("logtostderr", "true")
+}
+
+func main() {
+	var (
+		t   time.Time
+		res int
+	)
+	flag.Parse()
+	lines := strings.Split(input, "\n")
+
+	t = time.Now()
+	res = part1(lines)
+	glog.Infof("[Part 1] = %v", res)
+	glog.Infof("took %s", time.Since(t))
+
+	t = time.Now()
+	res = part2(lines)
+	glog.Infof("[Part 2] = %v", res)
+	glog.Infof("took %s", time.Since(t))
+}
---
+++ 2021/01/01_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var (
+	//go:embed in_test.txt
+	test string
+)
+
+func TestGeneric(t *testing.T) {
+	tests := []struct {
+	}{}
+	for _, tc := range tests {
+		t.Run("", func(t *testing.T) {
+			got := tc
+			want := tc
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  int
+	}{
+		{
+			input: strings.Split(test, "\n"),
+			want:  7,
+		},
+	}
+	for _, tc := range tests {
+		t.Run("", func(t *testing.T) {
+			got := part1(tc.input)
+			want := tc.want
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
