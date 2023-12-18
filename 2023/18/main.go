35013d418b878dfd409e8f963c367aa3a9e6d7c7
Dan Cohen <danrcohen@gmail.com>
2023-12-18 11:08:56 -0600

    refactor

--- 2023/18/main.go
+++ 2023/18/main.go
@@ -17,71 +17,73 @@ var (
 	input string
 )
 
-func solve(lines []string, fn func(line string) (n int, dir complex128)) int {
-	outer := 0
-	vertices := make([]complex128, 0, len(lines))
-	var v complex128
-	for _, line := range lines {
-		n, d := fn(line)
-		outer += n
+// a parseFn parses the space-separated fields of an instruction
+// and returns the number of meters to travel and the direction vector
+type parseFn func(fields []string) (n int, dir complex128)
+
+// findArea finds the area of a polygon using the Shoelace formula
+// https://en.wikipedia.org/wiki/Shoelace_formula
+// the vertices in the slice must be contiguous along the polygon
+func findArea(vertices []complex128) (area float64) {
+	n := len(vertices)
+	for i := 0; i < n; i++ {
+		v := vertices[i]
+		vv := vertices[(i+1)%n]
+		// trapezoid formula
+		area += real(v-vv) * imag(v+vv)
+	}
+	return math.Abs(area) / 2
+}
+
+func solve(instructions []string, parse parseFn) int {
+	var (
+		v         complex128
+		perimeter int
+		vertices  = make([]complex128, 0, len(instructions))
+	)
+
+	// follow instructions to find perimeter and all vertices
+	for _, line := range instructions {
+		n, d := parse(strings.Fields(line))
+		perimeter += n
 		v += d * complex(float64(n), 0)
 		vertices = append(vertices, v)
 	}
 
-	var area float64
+	area := findArea(vertices)
 
-	n := len(lines)
-	j := n - 1
-	for i := 0; i < n; i++ {
-		v := vertices[i]
-		vv := vertices[j]
-		area += real(v)*imag(vv) - real(vv)*imag(v)
-		j = i
-	}
-	area = math.Abs(area) / 2
-
-	// Pick's theorem to find # of inner points
-	// A - b/2 + 1 = i
-	inner := int(area - float64(outer)*0.5 + 1)
-	return outer + inner
+	// use Pick's theorem to find # of inner interior
+	// https://en.wikipedia.org/wiki/Pick%27s_theorem
+	// i = A - b/2 + 1
+	interior := int(area - float64(perimeter)*0.5 + 1)
+	return perimeter + interior
 }
 
 func part1(lines []string) (res int) {
-	return solve(lines, func(s string) (int, complex128) {
-		parts := strings.Fields(s)
-		n := util.MustAtoi(parts[1])
-		var d complex128
-		switch parts[0] {
-		case "R":
-			d = 1
-		case "L":
-			d = -1
-		case "U":
-			d = -1i
-		case "D":
-			d = 1i
-		}
-		return n, d
+	decoder := map[string]complex128{"R": 1, "L": -1, "U": -1i, "D": 1i}
+
+	return solve(lines, func(fields []string) (n int, dir complex128) {
+		n = util.MustAtoi(fields[1])
+
+		dir = decoder[fields[0]]
+		return
 	})
 }
 
 func part2(lines []string) (res int) {
-	return solve(lines, func(line string) (int, complex128) {
-		parts := strings.Fields(line)
-		hex := parts[2][2 : len(parts[2])-1]
-		n, _ := strconv.ParseInt(hex[:len(hex)-1], 16, 64)
-		var d complex128
-		switch hex[len(hex)-1] {
-		case '0':
-			d = 1
-		case '2':
-			d = -1
-		case '3':
-			d = -1i
-		case '1':
-			d = 1i
-		}
-		return int(n), d
+	decoder := map[byte]complex128{'0': 1, '1': 1i, '2': -1, '3': -1i}
+
+	return solve(lines, func(fields []string) (n int, dir complex128) {
+		// strip ()#
+		hex := strings.NewReplacer("(", "", ")", "", "#", "").Replace(fields[2])
+
+		// convert the first 5 digits
+		n64, _ := strconv.ParseInt(hex[:5], 16, 64)
+		n = int(n64)
+
+		// decode with the last digit
+		dir = decoder[hex[5]]
+		return
 	})
 }
 
