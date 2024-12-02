004021e4e5b4ec32c41d8e35fb94cbfae4729aa4
Dan Cohen <danrcohen@gmail.com>
2024-12-01 23:32:32 -0600

    ugly day 2

---
+++ 2024/02/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"math"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/venim/advent-of-code/util"
+)
+
+var (
+	//go:embed in.txt
+	input string
+)
+
+func toIntSlice(s []string) []int {
+	res := make([]int, len(s))
+	for i := 0; i < len(s); i++ {
+		res[i] = util.MustAtoi(s[i])
+	}
+	return res
+}
+
+func isSafe(levels []int) bool {
+	if !slices.IsSorted(levels) {
+		slices.Reverse(levels)
+		if !slices.IsSorted(levels) {
+			slices.Reverse(levels)
+			return false
+		}
+		slices.Reverse(levels)
+	}
+	for i := 1; i < len(levels); i++ {
+		delta := levels[i] - levels[i-1]
+		d := math.Abs(float64(delta))
+		if d < 1 || d > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func part1(lines []string) (res int) {
+	for _, l := range lines {
+		if isSafe(toIntSlice(strings.Fields(l))) {
+			res++
+		}
+	}
+	return
+}
+
+func removeLevel(l []int, i int) []int {
+	return slices.Delete(slices.Clone(l), i, i+1)
+}
+
+func part2(lines []string) (res int) {
+line:
+	for _, l := range lines {
+		levels := toIntSlice(strings.Fields(l))
+		if isSafe(levels) {
+			res++
+			continue
+		}
+		for i := 0; i < len(levels); i++ {
+			l := removeLevel(levels, i)
+			if isSafe(l) {
+				res++
+				continue line
+			}
+		}
+	}
+	return
+}
+
+func init() {
+	flag.Set("logtostderr", "true")
+}
+
+func main() {
+	var (
+		t   time.Time
+		res int
+	)
+	flag.Parse()
+	lines := strings.Split(input, "\r\n")
+
+	t = time.Now()
+	res = part1(lines)
+	glog.Infof("[Part 1] = %v", res)
+	glog.Infof("took %s", time.Since(t))
+
+	t = time.Now()
+	res = part2(lines)
+	glog.Infof("[Part 2] = %v", res)
+	glog.Infof("took %s", time.Since(t))
+}
---
+++ 2024/02/main_test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var (
+	//go:embed in_test.txt
+	test string
+)
+
+func TestGeneric(t *testing.T) {
+	tests := []struct {
+	}{}
+	for _, tc := range tests {
+		t.Run("", func(t *testing.T) {
+			got := tc
+			want := tc
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  int
+	}{
+		{
+			input: strings.Split(test, "\n"),
+			want:  2,
+		},
+		{
+			input: strings.Split(input, "\n"),
+			want:  -1,
+		},
+	}
+	for _, tc := range tests {
+		t.Run("", func(t *testing.T) {
+			got := part1(tc.input)
+			want := tc.want
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestRemoveLevel(t *testing.T) {
+	tests := []struct {
+		levels []int
+		index  int
+		want   []int
+	}{
+		{[]int{1, 3, 2, 4, 5}, 1, []int{1, 2, 4, 5}},
+	}
+	for _, tc := range tests {
+		t.Run("", func(t *testing.T) {
+			got := removeLevel(tc.levels, tc.index)
+			want := tc.want
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  int
+	}{
+		{
+			input: strings.Split(test, "\r\n"),
+			want:  4,
+		},
+		{
+			input: strings.Split(input, "\r\n"),
+			want:  626,
+		},
+	}
+	for _, tc := range tests {
+		t.Run("", func(t *testing.T) {
+			got := part2(tc.input)
+			want := tc.want
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
