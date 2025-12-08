ea778a155b8a3d33e8f3b182b423f09b11ebcd05
Dan Cohen <danrcohen@google.com>
2025-12-08 17:55:09 -0500

    gemini super-optimized

--- 2025/08/main.go
+++ 2025/08/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	_ "embed"
 	"flag"
 	"slices"
@@ -43,6 +44,11 @@ type Distance struct {
 	sq   int
 }
 
+// Less method for Distance to satisfy util.MinHeapItem interface
+func (d Distance) Less(other Distance) bool {
+	return d.sq < other.sq
+}
+
 func prepare(lines []string) ([]*JunctionBox, []Distance) {
 	var jboxes []*JunctionBox
 	for _, l := range lines {
@@ -61,20 +67,26 @@ func prepare(lines []string) ([]*JunctionBox, []Distance) {
 			})
 		}
 	}
-
-	slices.SortFunc(distances, func(a, b Distance) int {
-		return a.sq - b.sq
-	})
-
+	// Return unsorted distances. The heap will handle ordering.
 	return jboxes, distances
 }
 
-func run(jboxes []*JunctionBox, distances []Distance, cb func(step int, numComponents int, getSizes func() []int, getMergedGroup func() ([]int, int)) (int, bool)) int {
-	parent := make([]int, len(jboxes))
-	nodes := make([][]int, len(jboxes))
+func run(jboxes []*JunctionBox, allDistances []Distance, trackNodes bool, cb func(step int, numComponents int, sizes []int, getMergedGroup func() ([]int, int)) (int, bool)) int {
+	numJboxes := len(jboxes)
+	parent := make([]int, numJboxes)
+	sizes := make([]int, numJboxes)
+
+	var nodes [][]int
+	if trackNodes {
+		nodes = make([][]int, numJboxes)
+		for i := range jboxes {
+			nodes[i] = []int{i}
+		}
+	}
+
 	for i := range jboxes {
 		parent[i] = i
-		nodes[i] = []int{i}
+		sizes[i] = 1
 	}
 
 	find := func(i int) int {
@@ -91,9 +103,18 @@ func run(jboxes []*JunctionBox, distances []Distance, cb func(step int, numCompo
 		return root
 	}
 
-	numComponents := len(jboxes)
+	// Initialize Heap using util.MinHeap
+	// Copy distances to avoid modifying the original slice shared between parts
+	pq := make(util.MinHeap[Distance], len(allDistances))
+	copy(pq, allDistances)
+	heap.Init(&pq)
 
-	for step, d := range distances {
+	numComponents := numJboxes
+	step := 0
+
+	for pq.Len() > 0 {
+		d := heap.Pop(&pq).(Distance)
+
 		rootU := find(d.u)
 		rootV := find(d.v)
 
@@ -104,7 +125,8 @@ func run(jboxes []*JunctionBox, distances []Distance, cb func(step int, numCompo
 			var smallRoot, largeRoot int
 			var largeEndpoint int
 
-			if len(nodes[rootU]) < len(nodes[rootV]) {
+			// Union by size
+			if sizes[rootU] < sizes[rootV] {
 				smallRoot, largeRoot = rootU, rootV
 				largeEndpoint = d.v
 			} else {
@@ -113,45 +135,46 @@ func run(jboxes []*JunctionBox, distances []Distance, cb func(step int, numCompo
 			}
 
 			parent[smallRoot] = largeRoot
+			sizes[largeRoot] += sizes[smallRoot]
+			sizes[smallRoot] = 0 // The smaller component is now part of the larger one
+			// Only maintain node lists if required
+			if trackNodes {
+				lastMergedNodes = nodes[smallRoot]
+				bridgeNode = largeEndpoint
 
-			// Capture data for Part 2 before merging
-			lastMergedNodes = nodes[smallRoot]
-			bridgeNode = largeEndpoint
-
-			nodes[largeRoot] = append(nodes[largeRoot], nodes[smallRoot]...)
-			nodes[smallRoot] = nil // Release memory
+				nodes[largeRoot] = append(nodes[largeRoot], nodes[smallRoot]...)
+				nodes[smallRoot] = nil // Release memory
+			}
 
 			numComponents--
 		}
 
-		if res, done := cb(step, numComponents, func() []int {
-			sizes := make([]int, 0, numComponents)
-			for i, p := range parent {
-				if p == i && len(nodes[i]) > 0 {
-					sizes = append(sizes, len(nodes[i]))
-				}
-			}
-			return sizes
-		}, func() ([]int, int) {
+		if res, done := cb(step, numComponents, sizes, func() ([]int, int) {
 			return lastMergedNodes, bridgeNode
 		}); done {
 			return res
 		}
+		step++
 	}
 	return 0
 }
 
 func part1(lines []string, nTimes int) (res int) {
 	jboxes, distances := prepare(lines)
-	return run(jboxes, distances, func(step int, numComponents int, getSizes func() []int, getMergedGroup func() ([]int, int)) (int, bool) {
-		if step == nTimes-1 {
-			counts := getSizes()
-			slices.Sort(counts)
-			res = 1
-			start := len(counts) - 3
-			if start < 0 {
-				start = 0
+	// trackNodes is false for Part 1 as it doesn't need to know the specific nodes in each component, saving memory and time.
+	return run(jboxes, distances, false, func(step int, numComponents int, sizes []int, getMergedGroup func() ([]int, int)) (int, bool) {
+		if step == nTimes-1 { // Stop after nTimes connections
+			var counts []int
+			// Collect sizes of active components (those with size > 0, indicating they are roots)
+			for _, s := range sizes {
+				if s > 0 {
+					counts = append(counts, s)
+				}
 			}
+			slices.Sort(counts) // Sort component sizes to find the largest ones
+			res = 1
+			// Multiply the sizes of the 3 largest components
+			start := max(len(counts)-3, 0)
 			for i := len(counts) - 1; i >= start; i-- {
 				res *= counts[i]
 			}
@@ -163,10 +186,10 @@ func part1(lines []string, nTimes int) (res int) {
 
 func part2(lines []string) (res int) {
 	jboxes, distances := prepare(lines)
-	return run(jboxes, distances, func(step int, numComponents int, getSizes func() []int, getMergedGroup func() ([]int, int)) (int, bool) {
+	// trackNodes=true needed for logic
+	return run(jboxes, distances, true, func(step int, numComponents int, sizes []int, getMergedGroup func() ([]int, int)) (int, bool) {
 		if numComponents == 1 {
 			mergedNodes, bridge := getMergedGroup()
-			// Should always have a merge if we just hit 1 component
 			if mergedNodes != nil {
 				res = 1
 				for _, idx := range mergedNodes {
